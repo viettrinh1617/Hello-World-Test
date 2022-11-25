bff5df1dc680c93e1b8e61784df7de20fe95638f
viettrinh1617 <79010025+viettrinh1617@users.noreply.github.com>
2022-11-25 22:10:21 +0700

    to-do list

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-document.querySelector("body").innerHTML =  `<h1>Hello <span>World </span><strong>Viet</strong></h1>`;
+//Declare dom:
+const filterTask = document.querySelector("#filterBtn");
+const taskList = document.querySelector(".taskList");
+const taskInput = document.querySelector("#taskInput");
+const addTaskBtn = document.querySelector("#addTask");
+
+//filter tasks:
+filterTask.addEventListener("click", (e) => {
+    const taskItems = document.querySelectorAll(".taskList li");
+    taskItems.forEach(item => {
+        if(item.classList.contains("task-done")) {
+            item.style.display = "none";
+            if(e.target.checked) {item.style.display = "none";}
+            else {item.style.display = "flex";}
+        }
+    console.log(item.style.display);
+    });
+})
+
+//mark tasks as done and remove tasks:
+const markDone = (task) => {
+    task.classList.toggle("task-done");
+};
+
+const markDelete = (task) => {
+    task.classList.add("task-delete");
+    task.addEventListener("transitionend", () => {
+        task.style.display = "none";
+    })
+};
+
+taskList.addEventListener("click", (e)=> {
+    const element = e.target;
+    console.log(element);
+    if(element.classList.contains("checkbox-done")){
+        markDone(element.parentNode.parentNode);
+        console.log(element.parentNode.parentNode);
+    }
+    if(element.classList.contains("deleteTask")){
+        markDelete(element.parentNode.parentNode);
+        console.log(element.parentNode.parentNode);
+    }
+})
+
+//insert new tasks:
+addTaskBtn.addEventListener("click", (e) => {
+const x = document.createElement("li");
+x.innerHTML = `
+    <li class="task">
+    <div class="taskTitle">${taskInput.value}</div>
+    <div class="taskBtnGroup">
+        <input type="checkbox" class="checkbox checkbox-done">
+        <button class="deleteTask">X</button>
+    </div>
+    </li>
+`
+taskList.appendChild(x);
+taskInput.value = "";
+});
